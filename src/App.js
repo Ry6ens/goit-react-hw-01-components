7dcee084abf74e34220753b081f2894069289645
Sergey Gulaga <ry6ens@gmail.com>
2021-10-25 17:28:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Button } from 'react-bootstrap';
 import Profile from './components/Profile';
 import Statistics from './components/Statistics';
 import FriendList from './components/FriendList';
