e1018b1f632cee803a597fb209f076bdded82498
Sergey Gulaga <ry6ens@gmail.com>
2022-08-11 15:16:55 +0300

    add task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import Profile from './Profile/Profile';
+import user from './json/user.json';
+
+// const resolve = fetch(
+//   'https://raw.githubusercontent.com/goitacademy/react-homework/master/homework-01/social-profile/user.json'
+// );
+
+// const response = resolve.then(response => response.json());
+// const users = response.then(user => console.log(user));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </>
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,38 @@
+import s from './Profile.module.css';
+
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <>
+      <div className={s.profile}>
+        <div className={s.description}>
+          <img
+            src={avatar}
+            alt="User avatar"
+            className="avatar"
+            width="100px"
+          />
+          <p className={s.name}>{username}</p>
+          <p className="tag">@{tag}</p>
+          <p className="location">{location}</p>
+        </div>
+
+        <ul className={s.stats}>
+          <li className={s.item}>
+            <span className="label">Followers</span>
+            <span className="quantity">{stats.followers}</span>
+          </li>
+          <li className={s.item}>
+            <span className="label">Views</span>
+            <span className="quantity">{stats.views}</span>
+          </li>
+          <li className={s.item}>
+            <span className="label">Likes</span>
+            <span className="quantity">{stats.likes}</span>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
